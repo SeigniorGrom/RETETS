1225c7acd3aa1aa0fbde81c69cde497ed52917d2
alpha <alphawork0013@gmail.com>
2024-08-14 17:50:48 +0300

    ZabilKakNazuvaitsaComit

--- src/test/java/Alfa/Homework.java
+++ src/test/java/Alfa/Homework.java
@@ -1,4 +1,9 @@
 package Alfa;
 
 public class Homework {
+    public static void main(String[] args) {
+        System.out.println("Shabat Shalom"); //ln после принта делает переходит на другую строку //
+        System.out.print("Shabat"); // Принт без ln - остается на этой же строке //
+        System.out.print("Shalom");
+    }
 }
